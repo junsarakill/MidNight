4ab9984b90b8b0c411c5843d56224aac5f731222
junsarakill <61420467+junsarakill@users.noreply.github.com>
2024-08-20 09:29:48 +0900

    프로젝트 생성

---
+++ Source/MidNight/MidNightGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MidNightGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMidNightGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMidNightGameMode();
+};
+
+
+
