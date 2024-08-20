d8d747bcfbf9f8ba0d883a909beb6a980ab0ae1d
UmrealEngine <kr.suhyeok@gmail.com>
2024-08-20 23:07:18 +0900

    Top Default Moving

---
+++ Source/MidNight/Private/JSH_Point.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JSH_Point.h"
+
+// Sets default values
+AJSH_Point::AJSH_Point()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AJSH_Point::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AJSH_Point::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/MidNight/Public/JSH_FSM.h
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "JSH_FSM.generated.h"
+
+
+
+UENUM(BlueprintType)
+enum class TopState : uint8
+{
+	Idle,
+	Start,
+	Point01,
+	Point02,
+	Point03,
+};
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MIDNIGHT_API UJSH_FSM : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UJSH_FSM();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	TopState TState = TopState::Start;
+
+	// FSM 상태
+	void IdleState();
+	void StartState();
+	void Point01State();
+	void Point02State();
+	void Point03State();
+
+	// 한번만 실행되기 위한 Bool 값
+	bool one = true;
+	bool End01 = true;
+	bool End02 = true;
+	bool End03 = true;
+
+	// 플레이어
+	UPROPERTY()
+	class AJSH_Player* Player;
+
+	// 우클릭 시 저장되는 Name
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString ClickName;
+
+
+	// 이동 포인트 위치
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FSM)
+	class AJSH_Point* Point01;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FSM)
+	class AJSH_Point* Point02;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FSM)
+	class AJSH_Point* Point03;
+
+
+	UPROPERTY(EditAnywhere, Category=FSM)
+	float ReachDistance = 150.0f;
+
+
+
+	// 레벨 이동을 위한 함수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool OpenLevel03 = false;
+};
